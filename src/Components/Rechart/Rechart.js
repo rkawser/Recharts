fab706a680c80d919b14c80b994c3ab9096e6a50
raihan kawser <raihankawser55@gmail.com>
2022-10-12 11:02:30 +0600

    chart update

--- src/Components/Rechart/Rechart.js
+++ src/Components/Rechart/Rechart.js
@@ -1,69 +1,89 @@
 import React from 'react';
-import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Rechart = () => {
     const data = [
         {
-            name: 'Page A',
+            name: 'suplyer A',
             price: 2000,
-            pv: 2400,
+            month: 2,
+            pv: 200,
             amt: 2400,
         },
         {
-            name: 'Page B',
+            name: 'suplyer B',
             price: 3000,
-            pv: 1398,
+            month: 3,
+            pv: 198,
             amt: 2210,
         },
         {
-            name: 'Page C',
+            name: 'suplyer C',
             price: 2000,
-            pv: 9800,
+            month: 2,
+            pv: 980,
             amt: 2290,
         },
         {
-            name: 'Page D',
+            name: 'suplyer D',
             price: 2780,
             pv: 3908,
+            month: 2.5,
             amt: 2000,
         },
         {
-            name: 'Page E',
+            name: 'suplyer E',
             price: 1890,
+            month: 1.5,
             pv: 4800,
             amt: 2181,
         },
         {
-            name: 'Page F',
+            name: 'suplyer F',
             price: 2390,
-            pv: 3800,
+            month: 2.3,
+            pv: 300,
             amt: 2500,
         },
         {
-            name: 'Page G',
+            name: 'suplyer G',
             price: 3490,
+            month: 3.5,
             pv: 4300,
             amt: 2100,
         },
     ];
 
     return (
-        <div>
-            <div className='grid  grid-flow-col'>
-                <div className='h-96'>
-                    <LineChart width={400} height={500} data={data}>
-                       <Line dataKey={'price'}></Line>
-                       <Tooltip></Tooltip>
-                       <XAxis dataKey={'name'}></XAxis>
-                       <YAxis></YAxis>
+            <div className='grid  grid-flow-col mt-5 '>
+                <div className='h-96 '>
+                    <LineChart width={500} height={450} data={data}
+                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                        <CartesianGrid strokeDasharray="2 2" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="price" stroke="#8884d8" />
+                        <Line dataKey="month" stroke="#82c9d" />
+                        <Line type="monotone" dataKey="pv" stroke="#82ca9d"></Line>
+                        <Line dataKey={'amt'}></Line>
                     </LineChart>
                 </div>
-
-                <div >
-                    <h2>card one</h2>
+                
+                <div  className=''>
+                <BarChart width={600} height={250} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="price" fill="#8884d8" />
+                    <Bar dataKey="amt" fill="#82ca9d" />
+                    <Bar dataKey="pv" fill="#82ca9d" />
+                </BarChart>
                 </div>
             </div>
-        </div>
     );
 };
 
